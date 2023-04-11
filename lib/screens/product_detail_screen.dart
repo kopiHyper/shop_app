a5aeaf577ded1a360bb429ae354cc38c4c4b94bf
kopiHyper <oktawiankopica2001@gmail.com>
2023-04-11 21:08:32 +0200

    Adding navigation to the app

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+
+  // ProductDetailScreen(this.title);
+
+  static const routeName = '/detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId =
+        ModalRoute.of(context)!.settings.arguments; // It's the product id.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,36 +1,50 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imgUrl;
 
-  const ProductItem(this.id, this.title, this.imgUrl, {super.key});
+  const ProductItem(
+    this.id,
+    this.title,
+    this.imgUrl,
+  );
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          trailing: IconButton(
-              icon: const Icon(Icons.shopping_cart),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(
+            ProductDetailScreen.routeName,
+            arguments: id,
+          );
+        },
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            trailing: IconButton(
+                icon: const Icon(Icons.shopping_cart),
+                onPressed: () {},
+                color: Theme.of(context).colorScheme.secondary),
+            leading: IconButton(
+              icon: const Icon(Icons.favorite),
               onPressed: () {},
-              color: Theme.of(context).colorScheme.secondary),
-          leading: IconButton(
-            icon: const Icon(Icons.favorite),
-            onPressed: () {},
-            color: Theme.of(context).colorScheme.secondary,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
           ),
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
+          child: Image.network(
+            imgUrl,
+            fit: BoxFit.cover,
           ),
-        ),
-        child: Image.network(
-          imgUrl,
-          fit: BoxFit.cover,
         ),
       ),
     );
