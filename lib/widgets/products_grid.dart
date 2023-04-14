5cd931f223286d235798d3a1652dfe73983a1e70
kopiHyper <oktawiankopica2001@gmail.com>
2023-04-14 22:23:31 +0200

    Changed from create to value the ChangeNotifierProvider

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -14,8 +14,8 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (context, index) => ChangeNotifierProvider(
-        create: (context) => products[index],
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(
             // products[index].id,
             // products[index].title,
