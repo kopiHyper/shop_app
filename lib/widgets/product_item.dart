748033fc67cb7a05bcad95653e3060473d8116b2
kopiHyper <oktawiankopica2001@gmail.com>
2023-04-18 21:45:04 +0200

    Used Consumer for shink the rebuild area

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -17,7 +17,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
       child: GestureDetector(
@@ -34,13 +34,16 @@ class ProductItem extends StatelessWidget {
                 icon: const Icon(Icons.shopping_cart),
                 onPressed: () {},
                 color: Theme.of(context).colorScheme.secondary),
-            leading: IconButton(
-              icon: Icon(
-                  product.isFavourite ? Icons.favorite : Icons.favorite_border),
-              onPressed: () {
-                product.toggleFavouriteStatus();
-              },
-              color: Theme.of(context).colorScheme.secondary,
+            leading: Consumer<Product>(
+              builder: (ctx, product, child) => IconButton(
+                icon: Icon(product.isFavourite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                color: Theme.of(context).colorScheme.secondary,
+                onPressed: () {
+                  product.toggleFavouriteStatus();
+                },
+              ),
             ),
             title: Text(
               product.title,
