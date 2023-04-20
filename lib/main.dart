992170b991865c12c46acf95161b404b46e91733
kopiHyper <oktawiankopica2001@gmail.com>
2023-04-20 17:21:46 +0200

    Added a cart icon button that displays the amount of products in cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/products_provider.dart';
+
+import './providers/products_provider.dart';
+import './providers/cart.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -12,8 +14,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ProductsProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProductsProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
